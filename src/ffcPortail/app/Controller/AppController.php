2abb069841cbc06b9455de73b5f0cdad9b8f9988
sergeguban <sergeguban@gmail.com>
2018-12-14 05:09:01 +0100

    Prepare Year 2019

--- src/ffcPortail/app/Controller/AppController.php
+++ src/ffcPortail/app/Controller/AppController.php
@@ -35,7 +35,7 @@ class AppController extends Controller {
 
 	var $uses= array('Membership');
 	
-	public $currentYear = 2018;
+	public $currentYear = 2019;
 
 	public $components = array(
         'Session','Acl',
